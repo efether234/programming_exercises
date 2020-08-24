d1af7e8ed253a0bb771b845dfea4175fbeff723b
Evan Fetherolf <efether234@gmail.com>
2020-08-24 16:38:10 -0700

    Update docstrings for exercises.

--- hackerearth/zoos.py
+++ hackerearth/zoos.py
@@ -1,13 +1,30 @@
 '''
-# Sample code to perform I/O:
+You are required to enter a word that consists of 
+x
+ and 
+y
+ that denote the number of Zs and Os respectively. The input word is considered similar to word zoo if 
+2
+×
+x
+=
+y
+.
 
-name = input()                  # Reading input from STDIN
-print('Hi, %s.' % name)         # Writing output to STDOUT
+Determine if the entered word is similar to word zoo.
 
-# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
+
+Input format
+
+First line: A word that starts with several Zs and continues by several Os.
+Note: The maximum length of this word must be 
+20
+.
+Output format
+
+Print Yes if the input word can be considered as the string zoo otherwise, print No.
 '''
-
-# Write your code here
 
 word = input()
 x = 0
@@ -18,8 +35,6 @@ for letter in word:
     if letter == 'o':
         y += 1
 
-print(x)
-print(y)
 if 2 * x == y:
     print('Yes')
 else:
